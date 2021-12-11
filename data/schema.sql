0ea312004f4760e7cfbec2fa282425d6924d0289
Kevin Gil <kevingil23@gmail.com>
2021-12-11 11:49:03 -0800

    data schema

---
+++ data/schema.sql
@@ -0,0 +1,11 @@
+﻿-- Drop tables if exists
+DROP TABLE IF EXISTS clean_data_df
+
+-- Create data_clean table for raw dataset
+CREATE TABLE clean_data_df (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR,
+	author VARCHAR,
+	content VARCHAR,
+	label NUMERIC
+)
