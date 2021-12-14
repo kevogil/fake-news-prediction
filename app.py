8f2c93f11fbfe57c3fc6ca5fe90876d9cb4812d9
Vanessa Villagomez <villagomez.v@outlook.com>
2021-12-13 21:00:22 -0800

    update files

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Import flask and other libraries
-from models import create_classes
+#from models import create_classes
 import os
 from flask import (
     Flask,
@@ -7,14 +7,14 @@ from flask import (
     jsonify,
     request,
     redirect)
-import pandas as pd
+#import pandas as pd
 
 #from sqlalchemy import create_engine
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 # Use flask_pymongo to set up mongo connection
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', '').replace("postgres://", "postgresql://", 1)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', '').replace("postgres://", "postgresql://", 1)
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # create instance of Flask app
 app = Flask(__name__)
@@ -31,7 +31,7 @@ def visuals():
     return render_template("visuals.html")
 
 # Define what to do when a user hits the /model route
-@app.route("/model")
+@app.route("/model", methods=['POST', 'GET'])
 def model():
     return render_template("model.html")
 
