aac7eafc45272faa1c26bc519c1862f2572a0fdc
Vanessa Villagomez <villagomez.v@outlook.com>
2021-12-13 22:02:43 -0800

    update files and create classes

--- app.py
+++ app.py
@@ -12,12 +12,16 @@ from flask import (
 #from sqlalchemy import create_engine
 #from flask_sqlalchemy import SQLAlchemy
 
+# create instance of Flask app
+app = Flask(__name__)
+
 # Use flask_pymongo to set up mongo connection
 #app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL', '').replace("postgres://", "postgresql://", 1)
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# create instance of Flask app
-app = Flask(__name__)
+#db = SQLAlchemy(app)
+
+#News = create_classes(db)
 
 # create route that renders index.html template
 @app.route("/")
@@ -31,8 +35,11 @@ def visuals():
     return render_template("visuals.html")
 
 # Define what to do when a user hits the /model route
-@app.route("/model", methods=['POST', 'GET'])
+@app.route("/model") #, methods=['POST', 'GET']
 def model():
+    #if request.method == "POST":
+        #search_content = request.form['predictfakenews']
+    #else: 
     return render_template("model.html")
 
 # Run app
